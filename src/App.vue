247a5fe1ec552323bf5cd6a15a0ab761c6d52ea4
drinkingtheink <jason.m.harrison@gmail.com>
2024-09-24 16:09:28 -0500

    update base app

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,159 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <main>
+    <h1>Img Dark Mode Exploration</h1>
+
+    <div class="img">
+      <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" viewBox="0 0 302 296">
+        <defs>
+          <radialGradient id="radial-gradient" cx="-783.6" cy="713.5" fx="-783.6" fy="713.5" r="1" gradientTransform="translate(-23293 12705.8) rotate(90) scale(16.1 -33)" gradientUnits="userSpaceOnUse">
+            <stop offset=".6" stop-color="#6c6c6c"/>
+            <stop offset=".9" stop-color="#6c6c6c"/>
+          </radialGradient>
+          <radialGradient id="radial-gradient-2" cx="-788.5" cy="708.7" fx="-788.5" fy="708.7" r="1" gradientTransform="translate(-29091.4 15905) rotate(90) scale(20 -41.1)" gradientUnits="userSpaceOnUse">
+            <stop offset=".4" stop-color="#6c6c6c"/>
+            <stop offset=".9" stop-color="#6c6c6c"/>
+          </radialGradient>
+          <mask id="mask" x="128" y="50" width="51" height="20" maskUnits="userSpaceOnUse">
+            <g id="path-16-inside-1_1_142">
+              <rect class="cls-14" x="132" y="54" width="43" height="12" rx="2" ry="2"/>
+            </g>
+          </mask>
+          <mask id="mask-1" x="135" y="58" width="37" height="20" maskUnits="userSpaceOnUse">
+            <g id="path-17-inside-2_1_142">
+              <rect class="cls-14" x="139" y="62" width="29" height="12" rx="2" ry="2"/>
+            </g>
+          </mask>
+          <mask id="mask-2" x="59.8" y="66.8" width="45.2" height="45.2" maskUnits="userSpaceOnUse">
+            <g id="path-18-inside-3_1_142">
+              <rect class="cls-14" x="60.9" y="83.4" width="43" height="12" rx="2" ry="2" transform="translate(-39.1 84.5) rotate(-45)"/>
+            </g>
+          </mask>
+          <mask id="mask-3" x="70.4" y="77.4" width="35.3" height="35.3" maskUnits="userSpaceOnUse">
+            <g id="path-19-inside-4_1_142">
+              <rect class="cls-14" x="73.6" y="89.1" width="29" height="12" rx="2" ry="2" transform="translate(-41.4 90.2) rotate(-45)"/>
+            </g>
+          </mask>
+          <mask id="mask-4" x="202.5" y="66.8" width="45.2" height="45.2" maskUnits="userSpaceOnUse">
+            <g id="path-20-inside-5_1_142">
+              <rect class="cls-14" x="219.1" y="67.9" width="12" height="43" rx="2" ry="2" transform="translate(2.7 185.4) rotate(-45)"/>
+            </g>
+          </mask>
+          <mask id="mask-5" x="201.8" y="77.4" width="35.3" height="35.3" maskUnits="userSpaceOnUse">
+            <g id="path-21-inside-6_1_142">
+              <rect class="cls-14" x="213.4" y="80.6" width="12" height="29" rx="2" ry="2" transform="translate(-3 183) rotate(-45)"/>
+            </g>
+          </mask>
+        </defs>
+        <!-- Generator: Adobe Illustrator 28.7.1, SVG Export Plug-In . SVG Version: 1.2.0 Build 142)  -->
+        <g>
+          <g id="Layer_3">
+            <path class="cls-16" d="M257.4,235.6c19.8-23.6,31.7-53.9,31.7-87.1,0-74.9-60.7-135.6-135.6-135.6S17.8,73.6,17.8,148.5s11.9,63.5,31.7,87.1h207.9Z"/>
+            <path class="cls-16" d="M257.4,235.9c19.8-23.6,31.7-53.9,31.7-87.1,0-74.9-60.7-135.6-135.6-135.6S17.8,73.9,17.8,148.8s11.9,63.5,31.7,87.1h207.9Z"/>
+            <circle class="cls-19" cx="154" cy="148" r="135.4"/>
+            <path class="cls-17" d="M49.2,235.9c27.8,31.5,62.9,48.2,104.6,48.2s82.4-19.8,104-49"/>
+          </g>
+          <g id="Layer_1">
+            <path class="cls-9" d="M32.3,230.5c-15-22.1-23.7-47.9-25.1-74.6s4.4-53.2,16.9-76.8c12.5-23.6,31.2-43.3,54.1-57.1C101.1,8.3,127.4,1,154.1,1c26.7,0,52.9,7.3,75.8,21.1,22.9,13.8,41.6,33.5,54.1,57.2,12.5,23.6,18.3,50.2,16.8,76.9-1.5,26.7-10.2,52.4-25.2,74.5"/>
+            <path class="cls-9" d="M267.9,241c-13.8,16.9-31.2,30.5-50.9,39.9-19.7,9.3-41.3,14.2-63.1,14.2-21.8,0-43.4-4.9-63.1-14.3-19.7-9.4-37.1-23-50.8-39.9"/>
+            <circle class="cls-13" cx="154" cy="148" r="137"/>
+            <circle class="cls-18" cx="154" cy="148" r="137"/>
+            <path d="M24,234.5c-.8,0-1.5.7-1.5,1.5s.7,1.5,1.5,1.5v-3ZM24,237.5h261.5v-3H24v3Z"/>
+            <path class="cls-12" d="M275.4,141.1h-49c-13,0-6.3-18.2,1-15.6,1.6-8.8,12-6.2,15.1-3.6-4.7-4.7-.6-8.8-.6-8.8,6.8-8.3,21.9-2.6,22.5,6.2,7.3-2.1,11.9,1.6,13,7.8,10.9-2.1,9.9,14-2,14Z"/>
+            <path class="cls-7" d="M231.3,125.4c1.1-2.6,4.1-3.4,6.3-2.1"/>
+            <path class="cls-7" d="M245.5,115.7c1.5-3.4,9.3-4.5,13.1,1.1"/>
+            <path class="cls-5" d="M244,138.1h31.7"/>
+            <path class="cls-8" d="M71.2,156H10.2c-16.2,0-7.8-22.7,1.3-19.4,1.9-11,14.9-7.8,18.8-4.5-5.8-5.8-.7-11-.7-11,8.4-10.4,27.3-3.2,28,7.7,9.1-2.6,14.9,2,16.1,9.8,13.6-2.6,12.3,17.5-2.5,17.5Z"/>
+            <path class="cls-4" d="M16.3,136.5c1.4-3.3,5.1-4.2,7.8-2.6"/>
+            <path class="cls-4" d="M34,124.4c1.9-4.2,11.6-5.6,16.3,1.4"/>
+            <path class="cls-2" d="M32.2,152.3h39.5"/>
+            <circle class="cls-15" cx="154" cy="163" r="90"/>
+            <g class="cls-1">
+              <rect class="cls-27" x="132" y="54" width="43" height="12" rx="2" ry="2"/>
+            </g>
+            <g class="cls-25">
+              <rect class="cls-27" x="139" y="62" width="29" height="12" rx="2" ry="2"/>
+            </g>
+            <g class="cls-21">
+              <rect class="cls-26" x="60.9" y="83.4" width="43" height="12" rx="2" ry="2" transform="translate(-39.1 84.5) rotate(-45)"/>
+            </g>
+            <g class="cls-22">
+              <rect class="cls-26" x="73.6" y="89.1" width="29" height="12" rx="2" ry="2" transform="translate(-41.4 90.2) rotate(-45)"/>
+            </g>
+            <g class="cls-23">
+              <rect class="cls-26" x="219.1" y="67.9" width="12" height="43" rx="2" ry="2" transform="translate(2.7 185.4) rotate(-45)"/>
+            </g>
+            <g class="cls-24">
+              <rect class="cls-26" x="213.4" y="80.6" width="12" height="29" rx="2" ry="2" transform="translate(-3 183) rotate(-45)"/>
+            </g>
+            <path d="M150.5,98v2.5h5v-2.5h-5ZM155.5,87c0-1.4-1.1-2.5-2.5-2.5s-2.5,1.1-2.5,2.5h5ZM155.5,98v-11h-5v11h5Z"/>
+            <path d="M155.5,226v-2.5h-5v2.5h5ZM150.5,237c0,1.4,1.1,2.5,2.5,2.5s2.5-1.1,2.5-2.5h-5ZM150.5,226v11h5v-11h-5Z"/>
+            <path d="M217,159h-2.5v5h2.5v-5ZM228,164c1.4,0,2.5-1.1,2.5-2.5s-1.1-2.5-2.5-2.5v5ZM217,164h11v-5h-11v5Z"/>
+            <path d="M89,164.5h2.5v-5h-2.5v5ZM78,159.5c-1.4,0-2.5,1.1-2.5,2.5s1.1,2.5,2.5,2.5v-5ZM89,159.5h-11v5h11v-5Z"/>
+            <path class="cls-20" d="M183.8,105.1l-1.2,2.2,4.3,2.5,1.2-2.2-4.3-2.5ZM190.7,103.2c.7-1.2.3-2.7-.9-3.4-1.2-.7-2.7-.3-3.4.9l4.3,2.5ZM188.2,107.6l2.5-4.3-4.3-2.5-2.5,4.3,4.3,2.5Z"/>
+            <path class="cls-20" d="M119.9,107.6l1.2,2.2,4.3-2.5-1.2-2.2-4.3,2.5ZM121.7,100.8c-.7-1.2-2.2-1.6-3.4-.9-1.2.7-1.6,2.2-.9,3.4l4.3-2.5ZM124.2,105.1l-2.5-4.3-4.3,2.5,2.5,4.3,4.3-2.5Z"/>
+            <path class="cls-20" d="M97.1,131.7l2.2,1.2,2.5-4.3-2.2-1.3-2.5,4.3ZM95.2,124.8c-1.2-.7-2.7-.3-3.4.9-.7,1.2-.3,2.7.9,3.4l2.5-4.3ZM99.6,127.3l-4.3-2.5-2.5,4.3,4.3,2.5,2.5-4.3Z"/>
+            <path class="cls-20" d="M98.6,196.2l2.2-1.2-2.5-4.3-2.2,1.2,2.5,4.3ZM91.7,194.3c-1.2.7-1.6,2.2-.9,3.4.7,1.2,2.2,1.6,3.4.9l-2.5-4.3ZM96.1,191.8l-4.3,2.5,2.5,4.3,4.3-2.5-2.5-4.3Z"/>
+            <path class="cls-20" d="M123.7,219.2l1.2-2.2-4.3-2.5-1.2,2.2,4.3,2.5ZM116.8,221.1c-.7,1.2-.3,2.7.9,3.4,1.2.7,2.7.3,3.4-.9l-4.3-2.5ZM119.3,216.8l-2.5,4.3,4.3,2.5,2.5-4.3-4.3-2.5Z"/>
+            <path class="cls-20" d="M188.2,216.8l-1.2-2.2-4.3,2.5,1.2,2.2,4.3-2.5ZM186.3,223.6c.7,1.2,2.2,1.6,3.4.9,1.2-.7,1.6-2.2.9-3.4l-4.3,2.5ZM183.8,219.2l2.5,4.3,4.3-2.5-2.5-4.3-4.3,2.5Z"/>
+            <path class="cls-20" d="M210.2,191.8l-2.2-1.2-2.5,4.3,2.2,1.2,2.5-4.3ZM212.1,198.7c1.2.7,2.7.3,3.4-.9s.3-2.7-.9-3.4l-2.5,4.3ZM207.8,196.2l4.3,2.5,2.5-4.3-4.3-2.5-2.5,4.3Z"/>
+            <path class="cls-20" d="M207.8,127.3l-2.2,1.3,2.5,4.3,2.2-1.2-2.5-4.3ZM214.6,129.2c1.2-.7,1.6-2.2.9-3.4s-2.2-1.6-3.4-.9l2.5,4.3ZM210.2,131.7l4.3-2.5-2.5-4.3-4.3,2.5,2.5,4.3Z"/>
+            <path class="cls-10" d="M159.7,58.1h-78c-20.7,0-10-29,1.7-24.8,2.5-14.1,19.1-9.9,24.1-5.8-7.5-7.4-.9-14-.9-14,10.8-13.2,34.8-4.1,35.8,9.9,11.6-3.3,19,2.5,20.6,12.5,17.4-3.3,15.8,22.3-3.2,22.3Z"/>
+            <path class="cls-6" d="M89.6,33.1c1.8-4.2,6.5-5.3,10-3.4"/>
+            <path class="cls-6" d="M112.2,17.7c2.4-5.3,14.8-7.1,20.8,1.8"/>
+            <path class="cls-3" d="M109.8,53.3h50.5"/>
+            <path class="cls-28" d="M92,163.5c0-8.2,1.6-16.3,4.8-23.9,3.1-7.6,7.7-14.5,13.5-20.3,5.8-5.8,12.7-10.4,20.3-13.5,7.6-3.1,15.7-4.8,23.9-4.8v62.5h-62.5Z"/>
+            <path class="cls-11" d="M88,132.5l66,29.5h44"/>
+          </g>
+        </g>
+      </svg>`
+    </div>
+
+    <div>
+      <button @click="changeMode">CHANGE MODE</button>
+    </div>
+  </main>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  methods: {
+    changeMode() {
+      const body = document.body;
+
+      if (body.classList.contains('alt')) {
+        body.classList.remove('alt');
+      } else {
+        body.classList.add('alt');
+      }
+    }
   }
 }
 </script>
 
 <style>
+:root {
+  --base: #222;
+  --text: #eaeaea;
+}
+
+body {
+  background-color: var(--base);
+  color: var(--text);
+}
+
+p, h1, h2, h3, span {
+  color: var(--text);
+}
+
+body.alt {
+  background-color: var(--text);
+}
+
+body.alt p, body.alt h1, body.alt h2, body.alt h3, body.alt span {
+  color: var(--base);
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -22,5 +161,155 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+button {
+  padding: 1rem 3rem;
+  border-radius: 10px;
+}
+
+div.img {
+  max-width: 400px;
+  margin: 0 auto;
+}
+
+.cls-1 {
+  mask: url(#mask);
+}
+
+.cls-2, .cls-3, .cls-4, .cls-5, .cls-6, .cls-7 {
+  stroke: #969696;
+}
+
+.cls-2, .cls-3, .cls-4, .cls-5, .cls-6, .cls-7, .cls-8, .cls-9, .cls-10, .cls-11, .cls-12 {
+  stroke-linecap: round;
+}
+
+.cls-2, .cls-3, .cls-4, .cls-5, .cls-6, .cls-7, .cls-8, .cls-10, .cls-12 {
+  stroke-linejoin: round;
+}
+
+.cls-2, .cls-3, .cls-4, .cls-5, .cls-6, .cls-7, .cls-9, .cls-11, .cls-13 {
+  fill: none;
+}
+
+.cls-2, .cls-3, .cls-5 {
+  stroke-opacity: .2;
+}
+
+.cls-2, .cls-4 {
+  stroke-width: 1.4px;
+}
+
+.cls-3, .cls-6 {
+  stroke-width: 1.8px;
+}
+
+.cls-4, .cls-6, .cls-7 {
+  stroke-opacity: .4;
+}
+
+.cls-5, .cls-7 {
+  stroke-width: 1.1px;
+}
+
+.cls-8 {
+  stroke: url(#radial-gradient-2);
+  stroke-width: 1.9px;
+}
+
+.cls-8, .cls-10, .cls-14, .cls-15, .cls-12 {
+  fill: #fff;
+}
+
+.cls-9 {
+  stroke-width: 2px;
+}
+
+.cls-9, .cls-13 {
+  stroke: #a7a7a7;
+}
+
+.cls-10 {
+  stroke: #6c6c6c;
+  stroke-width: 2.4px;
+}
+
+.cls-16, .cls-17 {
+  fill-rule: evenodd;
+}
+
+.cls-16, .cls-18 {
+  display: none;
+}
+
+.cls-16, .cls-19 {
+  fill: #d8eaff;
+}
+
+.cls-17, .cls-18 {
+  fill: #049913;
+}
+
+.cls-20 {
+  fill: #9d9d9d;
+}
+
+.cls-21 {
+  mask: url(#mask-2);
+}
+
+.cls-22 {
+  mask: url(#mask-3);
+}
+
+.cls-23 {
+  mask: url(#mask-4);
+}
+
+.cls-24 {
+  mask: url(#mask-5);
+}
+
+.cls-25 {
+  mask: url(#mask-1);
+}
+
+.cls-15 {
+  stroke-width: 4px;
+}
+
+.cls-15, .cls-11, .cls-26, .cls-27 {
+  stroke: #000;
+}
+
+.cls-28 {
+  fill: #e39e46;
+}
+
+.cls-11 {
+  stroke-width: 5px;
+}
+
+.cls-13 {
+  stroke-width: 3px;
+}
+
+.cls-26 {
+  stroke-miterlimit: 4;
+  stroke-width: 8px;
+}
+
+.cls-26, .cls-27 {
+  fill: #afafaf;
+}
+
+.cls-12 {
+  stroke: url(#radial-gradient);
+  stroke-width: 1.5px;
+}
+
+.cls-27 {
+  stroke-width: 8px;
 }
 </style>
